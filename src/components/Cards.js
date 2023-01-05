de945317fe565e365fec688a2a56b8c2bb90eb9f
joshdidcote <joshua.didcote@outlook.com>
2023-01-05 20:55:37 +0000

    Add core game

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,27 +4,39 @@ function Card(props) {
   return (
     <div
       className="aspect-square m-8 text-lg font-bold border flex justify-center items-center"
-      onClick={() => props.shuffleCards()}
+      onClick={() => props.handleClick(props.id)}
     >
       Card: {props.id}
     </div>
   );
 }
 
-export default function Cards() {
+export default function Cards(props) {
   const [cards, setCards] = useState([...Array(8).keys()]);
+  const [clickedCards, setClickedCards] = useState([]);
 
   const shuffleCards = () => {
     var shuffledCards = [].concat(cards);
     shuffledCards.sort((a, b) => 0.5 - Math.random());
     setCards(shuffledCards);
-    console.log(cards);
+  };
+
+  const handleClick = (id) => {
+    if (clickedCards.includes(id)) {
+      alert("Game over");
+      props.setScore(0);
+      setClickedCards([]);
+      return;
+    }
+    props.setScore(props.score + 1);
+    shuffleCards();
+    setClickedCards([...clickedCards, id]);
   };
 
   return (
     <div className="grid grid-cols-4">
       {cards.map((card, i) => (
-        <Card shuffleCards={shuffleCards} key={card} id={card}>
+        <Card handleClick={handleClick} key={card} id={card}>
           test
         </Card>
       ))}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <div className="h-12 bg-slate-200 rounded-md">
       <div className="h-full flex justify-between px-8 items-center">
         <div className="text-2xl font-bold">Memory Game</div>
         <div className="h-full flex items-center font-bold">
-          <div className="mx-8">Current score:</div>
-          <div className="mx-8">Best score:</div>
+          <div className="mx-8">Current score: {props.score}</div>
+          <div className="mx-8">Best score: {props.bestScore}</div>
         </div>
       </div>
     </div>
