f246c2dc40fcc0c3fc8c58fb592035fa8339f8b3
joshdidcote <joshua.didcote@outlook.com>
2023-01-05 20:56:36 +0000

    Add score logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Header from "./components/Header";
 import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  }, [score]);
+
+  useEffect(() => console.log(score));
+
   return (
     <div className="min-h-screen mx-auto max-w-screen-lg pt-4">
-      <Header></Header>
-      <Cards></Cards>
+      <Header score={score} bestScore={bestScore}></Header>
+      <Cards score={score} setScore={setScore}></Cards>
     </div>
   );
 }
